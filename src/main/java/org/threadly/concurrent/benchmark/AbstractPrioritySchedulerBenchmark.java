699ee4fc392300ea0cb3852d02e8456a078a554e
Mike Jensen <jentfoo@gmail.com>
2015-08-11 22:51:49 -0600

    Only prestart threads if its the scheduler we are using

--- src/main/java/org/threadly/concurrent/benchmark/AbstractPrioritySchedulerBenchmark.java
+++ src/main/java/org/threadly/concurrent/benchmark/AbstractPrioritySchedulerBenchmark.java
@@ -4,11 +4,9 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 import org.threadly.concurrent.PriorityScheduler;
-import org.threadly.concurrent.SingleThreadScheduler;
 import org.threadly.concurrent.SubmitterSchedulerInterface;
 import org.threadly.concurrent.TaskPriority;
 
-@SuppressWarnings("unused")
 public abstract class AbstractPrioritySchedulerBenchmark extends AbstractBenchmark {
   protected static final int POOL_SIZE = 100;
   protected static final int RUNNABLE_COUNT = 10000;
@@ -23,7 +21,9 @@ public abstract class AbstractPrioritySchedulerBenchmark extends AbstractBenchma
   static {
     // change to StrictPriorityScheduler for testing logic (and then run inside eclipse)
     ORIGINAL_EXECUTOR = new PriorityScheduler(POOL_SIZE, TaskPriority.High, 0);
-    ORIGINAL_EXECUTOR.prestartAllThreads();
+    if (! USE_JAVA_EXECUTOR) {
+      ORIGINAL_EXECUTOR.prestartAllThreads();
+    }
     //EXECUTOR = ORIGINAL_EXECUTOR.makeSubPool(POOL_SIZE);
     EXECUTOR = ORIGINAL_EXECUTOR;
     //EXECUTOR = new SingleThreadScheduler();
@@ -33,7 +33,9 @@ public abstract class AbstractPrioritySchedulerBenchmark extends AbstractBenchma
   
   static {
     ScheduledThreadPoolExecutor jExecutor = new ScheduledThreadPoolExecutor(POOL_SIZE);
-    jExecutor.prestartAllCoreThreads();
+    if (USE_JAVA_EXECUTOR) {
+      jExecutor.prestartAllCoreThreads();
+    }
     JAVA_EXECUTOR = jExecutor;
     //JAVA_EXECUTOR = Executors.newSingleThreadScheduledExecutor();
   }
