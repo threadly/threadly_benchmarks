9d8b1a4259f78391380cb1218049e7e1b0bb1024
Mike Jensen <jentfoo@gmail.com>
2015-09-16 21:04:00 -0600

    Added more thread test cases

--- src/main/java/org/threadly/concurrent/benchmark/BenchmarkCollectionRunner.java
+++ src/main/java/org/threadly/concurrent/benchmark/BenchmarkCollectionRunner.java
@@ -36,7 +36,7 @@ public class BenchmarkCollectionRunner {
     ArrayList<BenchmarkCase> toRun = new ArrayList<>();
     
     String[] noArgs = new String[]{""};
-    String[] executeScheduleRecurringThreadCases = new String[]{"10", "50", "100", "200", "500"};
+    String[] executeScheduleRecurringThreadCases = new String[]{"4", "10", "20", "50", "100", "200", "500"};
     
     int benchmarkGroup = 0; // incremented for each one
     int classGroup = 0; // incremented at each class change
