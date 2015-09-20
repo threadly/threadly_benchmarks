fa8638493a7dd15d4a324b764046f5bdd0c80c62
Mike Jensen <jentfoo@gmail.com>
2015-09-20 17:01:59 -0600

    A lot WIDER range of tests across different thread counts

--- src/main/java/org/threadly/concurrent/benchmark/BenchmarkCollectionRunner.java
+++ src/main/java/org/threadly/concurrent/benchmark/BenchmarkCollectionRunner.java
@@ -36,7 +36,9 @@ public class BenchmarkCollectionRunner {
     ArrayList<BenchmarkCase> toRun = new ArrayList<>();
     
     String[] noArgs = new String[]{""};
-    String[] executeScheduleRecurringThreadCases = new String[]{"4", "10", "20", "50", "100", "200", "500"};
+    String[] executeScheduleRecurringThreadCases = new String[]{"4", "10", "20", "50", "100", "150", 
+                                                                "200", "250", "500", "750", "1000", 
+                                                                "1500", "2000", "2500"};
     
     int benchmarkGroup = 0; // incremented for each one
     int classGroup = 0; // incremented at each class change
