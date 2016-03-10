136555f6475a949c5879e468957df181a10fe040
Mike Jensen <jent@threadly.org>
2016-03-10 15:22:40 -0700

    Added "RateLimiterExecutorExecuteBenchmark"
    Not sure if these results are able to be depended on yet, so not included in the normal collection yet.

---
+++ src/main/java/org/threadly/concurrent/benchmark/RateLimiterExecutorExecuteBenchmark.java
@@ -0,0 +1,39 @@
+package org.threadly.concurrent.benchmark;
+
+import org.threadly.concurrent.PriorityScheduler;
+import org.threadly.concurrent.SubmitterScheduler;
+import org.threadly.concurrent.limiter.RateLimiterExecutor;
+
+public class RateLimiterExecutorExecuteBenchmark extends AbstractSchedulerExecuteBenchmark {
+  public static void main(String args[]) {
+    try {
+      new RateLimiterExecutorExecuteBenchmark(Integer.parseInt(args[0]), 
+                                              Integer.parseInt(args[1])).runTest();
+      System.exit(0);
+    } catch (Throwable t) {
+      t.printStackTrace();
+      System.exit(1);
+    }
+  }
+
+  protected final PriorityScheduler originalExecutor;
+  protected final SubmitterScheduler executor;
+  
+  public RateLimiterExecutorExecuteBenchmark(int threadRunTime, int poolSize) {
+    super(threadRunTime);
+    
+    originalExecutor = new PriorityScheduler(poolSize);
+    executor = new ExecutorSchedulerAdapter(new RateLimiterExecutor(originalExecutor, 
+                                                                    Double.MAX_VALUE));
+  }
+  
+  @Override
+  protected SubmitterScheduler getScheduler() {
+    return executor;
+  }
+
+  @Override
+  protected void shutdownScheduler() {
+    originalExecutor.shutdownNow();
+  }
+}
